5d1faf73fd7b4d2d9e8ee5ffef931af782629b09
Harsh Patel <code.by.hp@gmail.com>
2025-12-28 16:35:31 +0530

    implement pydantic schema for structured output

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from typing import List
+from pydantic import BaseModel, Field
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -7,16 +10,32 @@ from langchain_core.messages import HumanMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_tavily import TavilySearch
 
-llm=ChatGoogleGenerativeAI(model="gemini-2.5-flash")
-tools=[TavilySearch()]
-agent=create_agent(model=llm,tools=tools)
+class Source(BaseModel):
+    """Schema for a source used by the agent"""
+    url:str=Field(description="The URL of the source")
+
+class AgentResponse(BaseModel):
+    """Schema for agent response with answer and sources"""
+    answer:str=Field(description="The agent's answer to the query")
+    sources:List[Source]=Field(default_factory=list,description="List of sources used to generate the answer.")
+
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+tools = [TavilySearch()]
+agent = create_agent(model=llm, tools=tools,response_format=AgentResponse)
 
 
 def main():
     print("Hello from langchain-projects!")
-    result=agent.invoke({"messages":HumanMessage(content="search for 3 job postings for an ai engineer using langchain in Bhopal on linkedin and list their details")})
+    result = agent.invoke(
+        {
+            "messages": HumanMessage(
+                content="search for 3 job postings for an ai engineer using langchain in Bhopal on linkedin and list their details"
+            )
+        }
+    )
     print(result)
-    
+
 
 if __name__ == "__main__":
     main()
