15cf3657c8d6e2196884382f555e0461303cc251
Harsh Patel <code.by.hp@gmail.com>
2026-01-02 21:33:01 +0530

    implement agentic-rag fullflow

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def main():
-    print("Hello from langchain-projects!")
+from dotenv import load_dotenv
 
+load_dotenv()
+
+from graph.graph import app
 
 if __name__ == "__main__":
-    main()
+    print("hello advanced rag")
+    print(app.invoke(input={"question": "what is agent memory?"}))
